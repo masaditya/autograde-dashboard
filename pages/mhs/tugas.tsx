2cf6bb11fcd1870c8ccea79777d2ab81620bb1b2
masaditya <aeka489@gmail.com>
2021-06-10 19:45:47 +0700

    hokya

--- pages/mhs/tugas.tsx
+++ pages/mhs/tugas.tsx
@@ -1,20 +1,29 @@
 import { Accordion } from "components/Dashboard/Accordion";
 import { DashboardDiagram } from "components/Dashboard/DashboardDiagram";
 import { EXT_API } from "constant";
+import { useFetcher } from "lib/useFetcher";
+import { useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Tugasku() {
+  const [session, loading] = useSession();
+  const { getFetch } = useFetcher();
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    !loading &&
+      // @ts-ignore
+      getFetch("/assignment/" + session.user.id).then((res) => {
+        setData(res);
+        setIsLoading(false);
+      });
+  }, [loading]);
+
   return (
     <>
-      <Accordion />
+      <Accordion data={data} />
     </>
   );
 }
-
-// export async function getStaticProps() {
-//   const res = await (await fetch(EXT_API + "/assignment")).json();
-//   return {
-//     props: {
-//       tugas: res,
-//     },
-//   };
-// }
